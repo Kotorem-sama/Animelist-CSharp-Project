18876732a71b18a1a733d91310c7d1d2b2705eb2
Rick Slingerland <kotorem.sama@gmail.com>
2022-01-26 17:07:47 +0100

    Login page style is done

--- Animelist-CSharp-Project/Animelist-CSharp-Project/MainWindow.xaml.cs
+++ Animelist-CSharp-Project/Animelist-CSharp-Project/MainWindow.xaml.cs
@@ -24,5 +24,67 @@ namespace Animelist_CSharp_Project
         {
             InitializeComponent();
         }
+
+        private void TextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox source = e.Source as TextBox;
+            var brush = new SolidColorBrush(Color.FromArgb(255, (byte)165, (byte)165, (byte)165));
+            // Change the TextBox color when it obtains focus.
+            source.Background = brush;
+
+            if (source != null && (source.Text == "Password" || source.Text == "Username"))
+            {
+                // Clear the TextBox.
+                source.Clear();
+            }
+        }
+        private void TextBox_LostKeyboardFocusUsername(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox source = e.Source as TextBox;
+            var brush = new SolidColorBrush(Color.FromArgb(255, (byte)201, (byte)201, (byte)201));
+            source.Background = brush;
+            source.Text = source.Text.Trim();
+
+            if (source.Text == "")
+            {
+                source.Text = "Username";
+            }
+        }
+        private void TextBox_LostKeyboardFocusPassword(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox source = e.Source as TextBox;
+            var brush = new SolidColorBrush(Color.FromArgb(255, (byte)201, (byte)201, (byte)201));
+            source.Background = brush;
+            source.Text = source.Text.Trim();
+
+            if (source.Text == "")
+            {
+                source.Text = "Password";
+            }
+        }
+
+        public class User{
+            public string Username;
+            public string Password;
+
+            public User(string username, string password)
+            {
+                this.Username = username;
+                this.Password = password;
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User[] balls = { new User("User1", "password1"), new User("User2", "password2") };
+            for (int i = 0; i < balls.Length; i++)
+            {
+                if (UsernameTextBox.Text == balls[i].Username && PasswordTextBox.Text == balls[i].Password)
+                {
+
+                }
+            }
+        }
     }
 }
--- Animelist-CSharp-Project/Animelist-CSharp-Project/obj/Debug/Animelist-CSharp-Project_Content.g.i.cs
+++ Animelist-CSharp-Project/Animelist-CSharp-Project/obj/Debug/Animelist-CSharp-Project_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/bruh.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/pngegg%20(1).png")]
 
 
